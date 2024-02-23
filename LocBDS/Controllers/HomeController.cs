c0592d53e1971277d5c13fa61f3123da016eef03
Phuc <Bmphuc97@gmail.com>
2024-02-23 13:01:44 +0700

    test Upload file

--- LocBDS/Controllers/HomeController.cs
+++ LocBDS/Controllers/HomeController.cs
@@ -12,11 +12,13 @@ namespace LocBDS.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly RealEstateContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, RealEstateContext context)
+        public HomeController(ILogger<HomeController> logger, RealEstateContext context, IWebHostEnvironment hostingEnvironment)
         {
             _logger = logger;
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [AllowAnonymous]
@@ -75,6 +77,49 @@ namespace LocBDS.Controllers
         {
             return View();
         }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                // Lấy đường dẫn wwwroot + tên thư mục (uploads)
+                var uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+                
+                // Lấy tên file
+                string fileName = Path.GetFileName(file.FileName);
+
+                //Ghép đường dẫn Thư mục + tên file
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                // file chưa tồn tại mới đc lưu
+                if (!System.IO.File.Exists(filePath))
+                {
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+
+                ViewBag.Message = "File uploaded successfully";
+                ViewBag.FilePath = filePath;
+            }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return RedirectToAction("Upload");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
